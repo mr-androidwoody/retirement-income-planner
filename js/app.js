908584b65fd08d444b6743b972f26da200015682
mr-androidwoody <android.woody@gmail.com>
2026-03-08 12:29:17 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-import { initialiseTabs } from './ui/tabs.js';
+import { initialiseTabs, setActiveTab } from './ui/tabs.js';
 import { initialisePlanForm } from './ui/plan-form.js';
 import { initialiseAdvancedForm } from './ui/advanced-form.js';
 import { runRetirementSimulation } from './model/simulator.js';
@@ -8,21 +8,14 @@ import { renderResultsView } from './ui/results-view.js';
 let currentInputs = null;
 
 export function runSimulation(inputs) {
-
   currentInputs = inputs;
-
   const results = runRetirementSimulation(inputs);
-
   renderResultsView(results);
-
+  setActiveTab('results');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
   initialiseTabs({ defaultTab: 'plan' });
-
   initialisePlanForm(runSimulation);
-
   initialiseAdvancedForm();
-
 });
