4676cf76cadcb58c6aa33093c1dd60af1d2735f3
mr-androidwoody <android.woody@gmail.com>
2026-03-08 11:28:29 +0000

    Add files via upload

---
+++ js/model/worker.js
@@ -0,0 +1,19 @@
+import { runRetirementSimulation } from "../model/simulator.js";
+
+self.onmessage = (event) => {
+  const { type, inputs } = event.data || {};
+
+  if (type !== "run") {
+    return;
+  }
+
+  try {
+    const result = runRetirementSimulation(inputs);
+    self.postMessage({ ok: true, result });
+  } catch (error) {
+    self.postMessage({
+      ok: false,
+      error: error instanceof Error ? error.message : "Unknown worker error."
+    });
+  }
+};
