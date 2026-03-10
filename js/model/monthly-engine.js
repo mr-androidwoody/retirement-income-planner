c95ea09982ed7237a4ead47a8cee53b5e0ae3f30
mr-androidwoody <android.woody@gmail.com>
2026-03-10 07:52:27 +0000

    Add files via upload

--- js/model/monthly-engine.js
+++ js/model/monthly-engine.js
@@ -1,4 +1,10 @@
-import { initialiseBuckets, applyAssetReturns, withdrawFromBuckets, rebalanceBuckets, totalPortfolio } from './cashflow.js';
+import {
+  initialiseBuckets,
+  applyAssetReturns,
+  withdrawFromBuckets,
+  rebalanceBuckets,
+  totalPortfolio
+} from './cashflow.js';
 
 export function runMonthlyEngine(inputs) {
   const allocations = {
@@ -6,6 +12,7 @@ export function runMonthlyEngine(inputs) {
     bonds: inputs.bondAllocation,
     cashlike: inputs.cashlikeAllocation
   };
+
   let buckets = initialiseBuckets(inputs.initialPortfolio, allocations);
   let spending = inputs.initialSpending;
   let inflationIndex = 1;
@@ -15,23 +22,29 @@ export function runMonthlyEngine(inputs) {
     const monthIndex = month - 1;
     const yearIndex = Math.floor(monthIndex / 12);
     const monthInYear = monthIndex % 12;
+
     const monthlyReturns = {
       equities: Math.pow(1 + inputs.equityReturn, 1 / 12) - 1,
       bonds: Math.pow(1 + inputs.bondReturn, 1 / 12) - 1,
       cashlike: Math.pow(1 + inputs.cashlikeReturn, 1 / 12) - 1
     };
+
     const monthlyInflation = Math.pow(1 + inputs.inflation, 1 / 12) - 1;
 
     const start = totalPortfolio(buckets);
+
     const p1Eligible = inputs.person1Age + yearIndex >= inputs.person1PensionAge;
     const p2Eligible = inputs.person2Age + yearIndex >= inputs.person2PensionAge;
+
     const pensionAnnual =
       (p1Eligible ? inputs.person1PensionToday * inflationIndex : 0) +
       (p2Eligible ? inputs.person2PensionToday * inflationIndex : 0);
+
     const otherIncomeAnnual =
       yearIndex < inputs.otherIncomeYears
         ? inputs.otherIncomeToday * inflationIndex
         : 0;
+
     const windfallAnnual =
       monthInYear === 0 && inputs.windfallYear > 0 && yearIndex + 1 === inputs.windfallYear
         ? inputs.windfallAmount
@@ -40,12 +53,19 @@ export function runMonthlyEngine(inputs) {
     const pensionMonthly = pensionAnnual / 12;
     const otherIncomeMonthly = otherIncomeAnnual / 12;
     const windfallMonthly = windfallAnnual;
-    const spendingMonthly = spending / 12;
-    const totalIncomeMonthly = pensionMonthly + otherIncomeMonthly + windfallMonthly;
-    const withdrawal = Math.max(0, spendingMonthly - totalIncomeMonthly);
-    const actualWithdrawal = withdrawFromBuckets(buckets, withdrawal);
 
-    const surplusIncome = Math.max(0, totalIncomeMonthly - spendingMonthly);
+    const targetSpendingMonthly = spending / 12;
+    const totalIncomeMonthly = pensionMonthly + otherIncomeMonthly + windfallMonthly;
+
+    const requiredWithdrawal = Math.max(0, targetSpendingMonthly - totalIncomeMonthly);
+    const actualWithdrawal = withdrawFromBuckets(buckets, requiredWithdrawal);
+
+    const actualSpendingMonthly = Math.min(
+      targetSpendingMonthly,
+      totalIncomeMonthly + actualWithdrawal
+    );
+
+    const surplusIncome = Math.max(0, totalIncomeMonthly - targetSpendingMonthly);
     if (surplusIncome > 0) {
       buckets.cashlike += surplusIncome;
     }
@@ -60,7 +80,8 @@ export function runMonthlyEngine(inputs) {
       month,
       year: yearIndex + 1,
       startPortfolioNominal: start,
-      spendingNominal: spendingMonthly,
+      targetSpendingNominal: targetSpendingMonthly,
+      spendingNominal: actualSpendingMonthly,
       statePensionNominal: pensionMonthly,
       otherIncomeNominal: otherIncomeMonthly,
       windfallNominal: windfallMonthly,
@@ -70,6 +91,7 @@ export function runMonthlyEngine(inputs) {
     });
 
     inflationIndex *= 1 + monthlyInflation;
+
     if (monthInYear === 11) {
       spending *= 1 + inputs.inflation;
     }
